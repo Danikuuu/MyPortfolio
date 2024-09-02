c4faeea447e526edc8f3da63cc20aa6d68fcfd5e
Danikuuu <danikuroque@gmail.com>
2024-09-02 13:25:12 +0800

    display changes

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -39,6 +39,10 @@ const Grid = styled.div`
 display: grid;
 grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
 grid-gap: calc(1rem + 2vw);
+
+@media (max-width: 600px) { /* Adjust the max-width value as needed */
+grid-template-columns: 1fr;
+}
 `
 
 // Framer-motion config
